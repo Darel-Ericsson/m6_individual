0219b69e9f93adfa43c2841f36c9fde813dcda6c
Cristobal <cr.inzza@gmail.com>
2023-08-19 10:36:15 -0400

    add view user & tabla de usuarios

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "home/index.html")
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, "home/index.html",context)
 
 
 
