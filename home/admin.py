3fddd3e6c77236a41eea73f9059ad061562d415e
Cristobal <cr.inzza@gmail.com>
2023-08-19 09:39:06 -0400

    fix user fields admin site form

--- home/admin.py
+++ home/admin.py
@@ -3,4 +3,50 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2"
+                    ),
+            },
+        ),
+    )
+    fieldsets = (
+    (None, {"fields": ("username", "password")}),
+    (
+        ("Personal info"),
+        {
+            "fields": (
+                "first_name",
+                "last_name",
+                "rut",
+                "phone",
+                "birthday",
+                "email"
+            )
+        }
+    ),
+    (
+        ("Permissions"),
+        {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            ),
+        },
+    ),
+    (("Important dates"), {"fields": ("last_login", "date_joined")}),
+)
+
+
+admin.site.register(User, CustomUserAdmin)
