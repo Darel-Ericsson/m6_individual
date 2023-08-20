c1a73cac93c6df24b880b6d4c65e117be32a0dcc
Cristobal <cr.inzza@gmail.com>
2023-08-20 03:02:33 -0400

    rename home index y creacion index usuarios

--- home/views.py
+++ home/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from .models import User
 
 # Create your views here.
-def index(request):
+def home(request):
+    context = {}
+    return render(request, "home/home.html",context)
+
+def user_list(request):
     users = User.objects.all()
     context = {'users': users}
-    return render(request, "home/index.html",context)
+    return render(request, "user/index.html",context)
 
 
 
