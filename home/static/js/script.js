0c6d62248b4f27d950f1e0e384d4365d3329bb65
Cristobal <cr.inzza@gmail.com>
2023-08-20 09:30:43 -0400

    entrega individual 4

---
+++ home/static/js/script.js
@@ -0,0 +1,6 @@
+/* $(document).ready(function() {
+    // Agregar clases a los campos del formulario
+    $("#id_username").addClass("form-control");
+    $("#id_rut").addClass("form-control");
+    // Agrega más campos aquí según sea necesario
+}); */
